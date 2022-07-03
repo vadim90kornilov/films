3f99714aa96f0f66d6f383fc44e0832f5e814f14
Vadim <vandam90@yandex.ru>
2022-07-03 15:45:08 +0300

    3rd commit

--- js/script.js
+++ js/script.js
@@ -3,39 +3,65 @@ window.addEventListener('DOMContentLoaded', () => {
     const wrapper = document.querySelector('#wrapper');
     const image = document.querySelector('.img');
     const start = document.querySelector('#start');
-    let i = 0;
+    const button = document.querySelectorAll('[data = "btn"]');
+    const card = document.querySelector('.card');
+    let filmDB;
+    let counter = 0;
+    
 
-
+// отправка запроса на получение фильма
    async function fetchHandler () {
         try {
             const response = await fetch('http://localhost:3000/films');
-            const data = await response.json();
-            console.log(data[i]);
-            i++;
-            image.src = data[i].img;
-            
+            filmDB = await response.json();
+            showNextFilm  ();
 
         } catch (error) {
             console.log(error);
         }
     }
-    
+//начать игру    
     start.addEventListener('click', () => {
         fetchHandler();
+        start.classList.add('hide');
     }
   );
 
     wrapper.addEventListener('click', (event) => {
         if (event.target && event.target.tagName == 'BUTTON' ) {
+            showNextFilm ();
             console.log("Туц");
         }
     });
 
-    
+    function choiceAnswers () {
+       
+      
+    }
+
+    //показать следующий фильи
+    function showNextFilm () {
+        const i = random ();
+        image.src = filmDB[counter].img;
+        card.classList.add('fade');
+        button[i].innerText = filmDB[counter].title;
+        counter++;
+
+    }
 
 
+ //рандом от 1 до 4 
+ function random () {
+    const myArray = [0, 1, 2, 3];
+    const result =  Math.floor(Math.random()*myArray.length);
+    return myArray[result];
+ }
 
-
+ function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+ }
 
 
 
